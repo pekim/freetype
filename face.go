9cb5dccc29d49404f4bcf85c96e2089b0c859155
Mike D Pilsbury <mike.pilsbury@gmail.com>
2025-02-03 19:51:42 +0000

    Add Face.Reference method

--- face.go
+++ face.go
@@ -10,5 +10,10 @@ type Face struct {
 
 func (face Face) Done() error {
 	err := C.FT_Done_Face(face.face)
-	return newError(err, "failed to discard a face")
+	return newError(err, "failed to discard face")
+}
+
+func (face Face) Reference() error {
+	err := C.FT_Reference_Face(face.face)
+	return newError(err, "failed to reference face")
 }
