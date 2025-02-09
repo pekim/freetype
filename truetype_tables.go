8f296c572821d9c0d45d0fd12042274fc4640bbf
Mike D Pilsbury <mike.pilsbury@gmail.com>
2025-02-09 09:57:40 +0000

    WIP truetype tables

--- truetype_tables.go
+++ truetype_tables.go
@@ -4,7 +4,133 @@ package freetype
 // #include FT_FREETYPE_H
 //
 // #include <freetype/ttnameid.h>
+// #include <freetype/tttables.h>
 import "C"
+
+// TrueType-specific table types and functions.
+
+// TTHeader is a structure to model a TrueType font header table.
+// All fields follow the OpenType specification.
+// The 64-bit timestamps are stored in two-element arrays Created and Modified, first the upper then the lower 32 bits.
+//
+// https://freetype.org/freetype2/docs/reference/ft2-truetype_tables.html#tt_header
+type TTHeader struct {
+	Table_Version Fixed
+	Font_Revision Fixed
+
+	CheckSum_Adjust Long
+	Magic_Number    Long
+
+	Flags        UShort
+	Units_Per_EM UShort
+
+	created  [2]ULong
+	modified [2]ULong
+
+	XMin Short
+	YMin Short
+	XMax Short
+	YMax Short
+
+	MacStyle      UShort
+	LowestRecPPEM UShort
+
+	FontDirection    Short
+	IndexToLocFormat Short
+	GlyphDataFormat  Short
+}
+
+// func (header TTHeader)Created()[2]ULong{
+
+// }
+
+func init() {
+	assertSameSize(TTHeader{}, C.TT_Header{})
+}
+
+// TT_HoriHeader
+//
+//
+
+// TT_VertHeader
+//
+//
+
+// TT_OS2
+//
+//
+
+// TT_Postscript
+//
+//
+
+// TT_PCLT
+//
+//
+
+// TT_MaxProfile
+//
+//
+
+// FT_Sfnt_Tag
+//
+//
+
+// FT_Get_Sfnt_Table
+//
+//
+
+// FT_Load_Sfnt_Table
+//
+//
+
+// FT_Sfnt_Table_Info
+//
+//
+
+// FT_Get_CMap_Language_ID
+//
+//
+
+// FT_Get_CMap_Format
+//
+//
+
+// FT_PARAM_TAG_UNPATENTED_HINTING
+//
+//
+
+// TT_PLATFORM_XXX
+//
+//
+
+// TT_APPLE_ID_XXX
+//
+//
+
+// TT_MAC_ID_XXX
+//
+//
+
+// TT_ISO_ID_XXX
+//
+//
+
+// TT_MS_ID_XXX
+//
+//
+
+// TT_ADOBE_ID_XXX
+//
+//
+
+// TT_MAC_LANGID_XXX
+//
+//
+
+// TT_MS_LANGID_XXX
+//
+//
 
 // TTName is the possible values of the ‘name’ identifier field in the name records of an SFNT ‘name’ table.
 // These values are platform independent.
@@ -52,3 +178,7 @@ const (
 	/* This is new in OpenType 1.8 */
 	NAME_ID_VARIATIONS_PREFIX = TTName(C.TT_NAME_ID_VARIATIONS_PREFIX)
 )
+
+// TT_UCR_XXX
+//
+//
