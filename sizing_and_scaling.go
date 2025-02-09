c891949525b9c32d6acfdd5e08ab51261701136e
Mike D Pilsbury <mike.pilsbury@gmail.com>
2025-02-09 19:16:35 +0000

    Implement sizing and scaling functions and types

--- sizing_and_scaling.go
+++ sizing_and_scaling.go
@@ -71,110 +71,108 @@ type BitmapSize struct {
 	YPpem Pos
 }
 
-// /*
-// SetCharSize requests the nominal size (in points).
+/*
+SetCharSize requests the nominal size (in points).
 
-// https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_set_char_size
-// */
-// func (face Face) SetCharSize(
-// 	charWidth F26Dot6, charHeight F26Dot6,
-// 	horzResolution UInt, vertResolution UInt,
-// ) error {
-// 	err := C.FT_Set_Char_Size(face.face, charWidth, charHeight, horzResolution, vertResolution)
-// 	return newError(err, "failed to set char size for face")
-// }
+https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_set_char_size
+*/
+func (face Face) SetCharSize(
+	charWidth F26Dot6, charHeight F26Dot6,
+	horzResolution UInt, vertResolution UInt,
+) error {
+	err := libfreetype.XFT_Set_Char_Size(face.tls, face.face, charWidth, charHeight, horzResolution, vertResolution)
+	return newError(err, "failed to set char size for face")
+}
 
-// /*
-// SetPixelSizes requests the nominal size (in pixels).
+/*
+SetPixelSizes requests the nominal size (in pixels).
 
-// https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_set_pixel_sizes
-// */
-// func (face Face) SetPixelSizes(pixelWidth UInt, pixelHeight UInt) error {
-// 	err := C.FT_Set_Pixel_Sizes(face.face, pixelWidth, pixelHeight)
-// 	return newError(err, "failed to set pixel sizes for face")
-// }
+https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_set_pixel_sizes
+*/
+func (face Face) SetPixelSizes(pixelWidth UInt, pixelHeight UInt) error {
+	err := libfreetype.XFT_Set_Pixel_Sizes(face.tls, face.face, pixelWidth, pixelHeight)
+	return newError(err, "failed to set pixel sizes for face")
+}
 
-// /*
-// RequestSize resizes the scale of the active Size object in a face.
+/*
+RequestSize resizes the scale of the active Size object in a face.
 
-// https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_request_size
-// */
-// func (face Face) RequestSize(req SizeRequestRec) error {
-// 	err := C.FT_Request_Size(face.face, toPointer[C.FT_Size_RequestRec](req))
-// 	return newError(err, "failed to request size for face")
-// }
+https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_request_size
+*/
+func (face Face) RequestSize(req SizeRequestRec) error {
+	err := libfreetype.XFT_Request_Size(face.tls, face.face, toUintptr(&req))
+	return newError(err, "failed to request size for face")
+}
 
-// /*
-// SelectSize selects a bitmap strike.
+/*
+SelectSize selects a bitmap strike.
 
-// https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_select_size
-// */
-// func (face Face) SelectSize(strikeIndex Int) error {
-// 	err := C.FT_Select_Size(face.face, strikeIndex)
-// 	return newError(err, "failed to set select size for face")
-// }
+https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_select_size
+*/
+func (face Face) SelectSize(strikeIndex Int) error {
+	err := libfreetype.XFT_Select_Size(face.tls, face.face, strikeIndex)
+	return newError(err, "failed to set select size for face")
+}
 
-// /*
-// SizeRequestType is an enumeration type that lists the supported size request types,
-// i.e., what input size (in font units) maps to the requested output size (in pixels,
-// as computed from the arguments of SizeRequest).
+/*
+SizeRequestType is an enumeration type that lists the supported size request types,
+i.e., what input size (in font units) maps to the requested output size (in pixels,
+as computed from the arguments of SizeRequest).
 
-// https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_size_request_type
-// */
-// type SizeRequestType = C.FT_Size_Request_Type
+https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_size_request_type
+*/
+type SizeRequestType = libfreetype.TFT_Size_Request_Type
 
-// const (
-// 	SIZE_REQUEST_TYPE_NOMINAL  = SizeRequestType(C.FT_SIZE_REQUEST_TYPE_NOMINAL)
-// 	SIZE_REQUEST_TYPE_REAL_DIM = SizeRequestType(C.FT_SIZE_REQUEST_TYPE_REAL_DIM)
-// 	SIZE_REQUEST_TYPE_BBOX     = SizeRequestType(C.FT_SIZE_REQUEST_TYPE_BBOX)
-// 	SIZE_REQUEST_TYPE_CELL     = SizeRequestType(C.FT_SIZE_REQUEST_TYPE_CELL)
-// 	SIZE_REQUEST_TYPE_SCALES   = SizeRequestType(C.FT_SIZE_REQUEST_TYPE_SCALES)
-// )
+const (
+	SIZE_REQUEST_TYPE_NOMINAL  = SizeRequestType(0)
+	SIZE_REQUEST_TYPE_REAL_DIM = SizeRequestType(1)
+	SIZE_REQUEST_TYPE_BBOX     = SizeRequestType(2)
+	SIZE_REQUEST_TYPE_CELL     = SizeRequestType(3)
+	SIZE_REQUEST_TYPE_SCALES   = SizeRequestType(4)
+)
 
-// func init() {
-// 	assertSameSize(SizeRequestRec{}, C.FT_Size_RequestRec{})
-// }
+func init() {
+	assertSameSize(SizeRequestRec{}, libfreetype.TFT_Size_RequestRec{})
+}
 
-// /*
-// SizeRequestRec is a structure to model a size request.
+/*
+SizeRequestRec is a structure to model a size request.
 
-// https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_size_requestrec
-// */
-// type SizeRequestRec struct {
-// 	Type           SizeRequestType
-// 	Width          Long
-// 	Height         Long
-// 	HoriResolution UInt
-// 	VertResolution UInt
-// }
+https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_size_requestrec
+*/
+type SizeRequestRec struct {
+	Type           SizeRequestType
+	Width          Long
+	Height         Long
+	HoriResolution UInt
+	VertResolution UInt
+}
 
-// // SizeRequest is a handle to a size request structure.
-// //
-// // https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_size_request
-// type SizeRequest *SizeRequestRec
+// SizeRequest is a handle to a size request structure.
+//
+// https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_size_request
+type SizeRequest *SizeRequestRec
 
-// /*
-// SetTransform sets the transformation that is applied to glyph images when they are loaded into a
-// glyph slot through FT_Load_Glyph.
+/*
+SetTransform sets the transformation that is applied to glyph images when they are loaded into a
+glyph slot through FT_Load_Glyph.
 
-// https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_set_transform
-// */
-// func (face Face) SetTransform(matrix *Matrix, delta *Vector) {
-// 	C.FT_Set_Transform(face.face,
-// 		(*C.FT_Matrix)(unsafe.Pointer(matrix)),
-// 		(*C.FT_Vector)(unsafe.Pointer(delta)),
-// 	)
-// }
+https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_set_transform
+*/
+func (face Face) SetTransform(matrix *Matrix, delta *Vector) {
+	libfreetype.XFT_Set_Transform(face.tls, face.face, toUintptr(matrix), toUintptr(delta))
+}
 
-// /*
-// GetTransform returns the transformation that is applied to glyph images when they are loaded into
-// a glyph slot through Load_Glyph. See SetTransform for more details.
+/*
+GetTransform returns the transformation that is applied to glyph images when they are loaded into
+a glyph slot through Load_Glyph. See SetTransform for more details.
 
-// https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_get_transform
-// */
+https://freetype.org/freetype2/docs/reference/ft2-sizing_and_scaling.html#ft_get_transform
+*/
+// // TODO - see https://gitlab.com/cznic/libfreetype/-/issues/1
 // func (face Face) GetTransform() (Matrix, Vector) {
-// 	var matrix C.FT_Matrix
-// 	var vector C.FT_Vector
-// 	C.FT_Get_Transform(face.face, &matrix, &vector)
-// 	return to[Matrix](matrix), to[Vector](vector)
+// 	var matrix Matrix
+// 	var vector Vector
+// 	libfreetype.XFT_Get_Transform(face.tls,face.face, &matrix, &vector)
+// 	return matrix, vector
 // }
---
+++ sizing_and_scaling_test.go
@@ -0,0 +1,56 @@
+package freetype
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/pekim/freetype-go/internal/font"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFaceSetCharSize(t *testing.T) {
+	lib, _ := Init()
+	face, _ := lib.NewMemoryFace(font.DejaVuSansMono, 0)
+	err := face.SetCharSize(50, 50, 96, 96)
+	assert.Nil(t, err)
+}
+
+func TestFaceSetPixelSizes(t *testing.T) {
+	lib, _ := Init()
+	face, _ := lib.NewMemoryFace(font.DejaVuSansMono, 0)
+	err := face.SetPixelSizes(24, 0)
+	assert.Nil(t, err)
+}
+
+func TestFaceRequestSize(t *testing.T) {
+	lib, _ := Init()
+	face, _ := lib.NewMemoryFace(font.DejaVuSansMono, 0)
+	err := face.RequestSize(SizeRequestRec{
+		Type:           SIZE_REQUEST_TYPE_BBOX,
+		Width:          50,
+		Height:         50,
+		HoriResolution: 96,
+		VertResolution: 96,
+	})
+	assert.Nil(t, err)
+}
+
+func TestFaceSelectSize(t *testing.T) {
+	lib, _ := Init()
+	face, _ := lib.NewMemoryFace(font.DejaVuSansMono, 0)
+	err := face.SelectSize(1)
+	assert.Error(t, err) // the font is not a bitmap font
+}
+
+func TestFaceGetTransformSetTransform(_ *testing.T) {
+	lib, _ := Init()
+	face, _ := lib.NewMemoryFace(font.DejaVuSansMono, 0)
+
+	matrix := Matrix{XX: 1, XY: 2, YX: 3, YY: 4}
+	vector := Vector{X: 5, Y: 6}
+	face.SetTransform(&matrix, &vector)
+
+	// matrix2, vector2 := face.GetTransform()
+	// assert.Equal(t, matrix, matrix2)
+	// assert.Equal(t, vector, vector2)
+}
