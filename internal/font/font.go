621c7580db0d99d5d8f98b8a9ff2512cebf1e85f
Mike D Pilsbury <mike.pilsbury@gmail.com>
2025-02-16 19:59:02 +0000

    Add a test for Library.SetSVGHooks

--- internal/font/font.go
+++ internal/font/font.go
@@ -7,3 +7,6 @@ var DejaVuSans []byte
 
 //go:embed DejaVuSans/DejaVuSansMono.ttf
 var DejaVuSansMono []byte
+
+//go:embed Noto_Color_Emoji/NotoColorEmoji-Regular.ttf
+var NotoColorEmoji []byte
---
+++ opentype_svg_fonts_test.go
@@ -0,0 +1,65 @@
+package freetype
+
+import (
+	"testing"
+
+	"github.com/pekim/freetype/internal/font"
+	"github.com/stretchr/testify/assert"
+	"modernc.org/libc"
+)
+
+func TestLibrarySetSVGHooks(t *testing.T) {
+	initFuncCalled := false
+	var initFunc SVGLibInitFunc = func(_ *libc.TLS, _ uintptr) FTError {
+		initFuncCalled = true
+		return Err_Ok
+	}
+
+	freeFuncCalled := false
+	var freeFunc SVGLibFreeFunc = func(_ *libc.TLS, _ uintptr) {
+		freeFuncCalled = true
+	}
+
+	renderFuncCalled := false
+	var renderFunc SVGLibRenderFunc = func(_ *libc.TLS, _ GlyphSlot, _ uintptr) FTError {
+		renderFuncCalled = true
+		return Err_Ok
+	}
+
+	presetSlotFuncCalled := 0
+	var presetSlotFunc SVGLibPresetSlotFunc = func(_ *libc.TLS, _ GlyphSlot, _ Bool, _ uintptr) FTError {
+		presetSlotFuncCalled++
+		return Err_Ok
+	}
+
+	lib, err := Init()
+	assert.NoError(t, err)
+
+	err = lib.SetSVGHooks(SVGRendererHooks{
+		InitSVG:    initFunc,
+		FreeSvg:    freeFunc,
+		RenderSVG:  renderFunc,
+		PresetSlot: presetSlotFunc,
+	})
+	assert.NoError(t, err)
+
+	face, err := lib.NewMemoryFace(font.NotoColorEmoji, 0)
+	assert.NoError(t, err)
+
+	err = face.SetPixelSizes(0, 32)
+	assert.NoError(t, err)
+
+	err = face.LoadGlyph(face.GetCharIndex('😀'), LOAD_COLOR)
+	assert.NoError(t, err)
+
+	err = face.RenderGlyph(RENDER_MODE_NORMAL)
+	assert.NoError(t, err)
+
+	err = lib.Done()
+	assert.NoError(t, err)
+
+	assert.True(t, initFuncCalled)
+	assert.True(t, freeFuncCalled)
+	assert.True(t, renderFuncCalled)
+	assert.Equal(t, 2, presetSlotFuncCalled)
+}
